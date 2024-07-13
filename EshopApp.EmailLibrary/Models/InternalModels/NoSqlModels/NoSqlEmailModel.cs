035dc90ac32fdbc5d8f453145111a25a186d4512
konstantians <kinnaskonstantinos0@gmail.com>
2024-07-13 14:58:16 +0300

    Added Email Library Functionality
    With this update the email library should be able to be used to send emails and to store email entries in the database. The database could either be cosmodb or sqlserver and that is determined by the appsettings of the module that uses the library. Logging has also been added for all crucial funtionality. Testing and integration with the REST Email REST API will be done in a future commit.

---
+++ EshopApp.EmailLibrary/Models/InternalModels/NoSqlModels/NoSqlEmailModel.cs
@@ -0,0 +1,24 @@
+﻿using EshopApp.EmailLibrary.Models.RequestModels;
+namespace EshopApp.EmailLibrary.Models.InternalModels.NoSqlModels;
+
+internal class NoSqlEmailModel
+{
+    public string? Id { get; set; }
+    public DateTime SentAt { get; set; }
+    public string? Receiver { get; set; }
+    public string? Title { get; set; }
+    public string? Message { get; set; }
+
+    public NoSqlEmailModel()
+    {
+
+    }
+
+    public NoSqlEmailModel(EmailRequestModel emailRequestModel)
+    {
+        Receiver = emailRequestModel.Receiver;
+        Title = emailRequestModel.Title;
+        Message = emailRequestModel.Message;
+    }
+
+}
---
+++ EshopApp.EmailLibrary/Models/ResponseModels/EmailResponseModel.cs
@@ -0,0 +1,34 @@
+﻿using EshopApp.EmailLibrary.Models.InternalModels.NoSqlModels;
+using EshopApp.EmailLibrary.Models.InternalModels.SqlModels;
+
+namespace EshopApp.EmailLibrary.Models.ResponseModels;
+
+public class EmailResponseModel
+{
+    public string? Id { get; set; }
+    public DateTime SentAt { get; set; }
+    public string? Receiver { get; set; }
+    public string? Title { get; set; }
+    public string? Message { get; set; }
+
+    public EmailResponseModel() { }
+
+    internal EmailResponseModel(SqlEmailModel sqlEmailModel)
+    {
+        Id = sqlEmailModel.Id;
+        SentAt = sqlEmailModel.SentAt;
+        Receiver = sqlEmailModel.Receiver;
+        Title = sqlEmailModel.Title;
+        Message = sqlEmailModel.Message;
+    }
+
+    internal EmailResponseModel(NoSqlEmailModel noSqlEmailModel)
+    {
+        Id = noSqlEmailModel.Id;
+        SentAt = noSqlEmailModel.SentAt;
+        Receiver = noSqlEmailModel.Receiver;
+        Title = noSqlEmailModel.Title;
+        Message = noSqlEmailModel.Message;
+    }
+
+}
