3cb8dc35569f011219334cb048b8b5c74bc50a8f
konstantians <kinnaskonstantinos0@gmail.com>
2024-12-18 10:04:14 +0200

    Added Authorization Endpoints In The Gateway Api
    I added all the necessary authorization endpoints that are needed to cover the admincontroller of the Auth microservice(CRUD operations that the admin can access). I also fixed a bug that made the endpoints of the gateway api to only sent health checks to the auth microservice. When I start working on the Data controllers of the gateway api I will also add some logic for the cart and the user coupons, but as it stands I will leave it as it is. On a final note maybe I should add in app notifications(for userupdate and other stuff). I am not certain about this yet.

--- EshopApp.AuthLibraryAPI/Models/RequestModels/AdminModels/ApiUpdateUserRequestModel.cs
+++ EshopApp.AuthLibraryAPI/Models/RequestModels/AdminModels/ApiUpdateUserRequestModel.cs
@@ -1,9 +1,11 @@
 ﻿using EshopApp.AuthLibrary.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace EshopApp.AuthLibraryAPI.Models.RequestModels.AdminModels;
 
 public class ApiUpdateUserRequestModel
 {
+    [Required]
     public AppUser? AppUser { get; set; }
     public string? Password { get; set; }
     public bool ActivateEmail { get; set; } = true;
---
+++ EshopApp.GatewayAPI/Models/RequestModels/GatewayAdminControllerRequestModels/GatewayApiUpdateUserRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EshopApp.GatewayAPI.Models.RequestModels.GatewayAdminControllerRequestModels;
+
+public class GatewayApiUpdateUserRequestModel
+{
+    [Required]
+    public GatewayAppUser? AppUser { get; set; }
+    public bool ActivateEmail { get; set; }
+    public string? Password { get; set; }
+}
